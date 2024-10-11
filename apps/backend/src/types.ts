417cf8a233d8db17e0e99a95c7c168f36c5743b6
Mateusz <mateusz-wojcik@hotmail.com>
2024-10-11 21:49:48 +0200

    [SCA-2]: types for metadata and xml structure

--- apps/backend/src/types.ts
+++ apps/backend/src/types.ts
@@ -4,3 +4,54 @@ export interface UpdatePayload {
   name: string;
   newValue: string;
 }
+
+export interface XmlFileMetaData {
+  blockVersion: string;
+  majorVersion: string;
+  minorVersion: string;
+  date: string;
+}
+
+export interface VersionAttributes {
+  MajorVersion: string;
+  MinorVersion: string;
+  RebuildType: string;
+  RebuildId: string;
+  AffectedEcus: string;
+  LicenseSerialNumber: string;
+  ApplicationId: string;
+  ApplicationVersion: string;
+  Date: string;
+}
+
+export interface VersionBlock {
+  $: { Version: string };
+  Version: Array<{ $: VersionAttributes }>;
+}
+
+export interface FPC {
+  $: {
+    Name: string;
+    Value: string;
+    Updated: string;
+  };
+}
+
+export interface FpcBlock {
+  $: { Version: string };
+  FPC: FPC[];
+}
+
+export interface Data {
+  FpcBlock: FpcBlock[];
+  VersionBlock: VersionBlock[];
+}
+
+export interface Sops {
+  Meta: object;
+  Data: Data[];
+}
+
+export interface ParsedXml {
+  Sops: Sops;
+}
