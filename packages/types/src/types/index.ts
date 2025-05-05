593230a16fe075af67c7d05fdc5527aaad42d273
Mateusz <mateusz-wojcik@hotmail.com>
2025-05-05 20:35:58 +0200

    [SCA-29] cable list service, shared types

--- packages/types/src/types/index.ts
+++ packages/types/src/types/index.ts
@@ -5,3 +5,4 @@ export { XmlFileMetaData } from './xmlFileMetaData.types';
 export { UpdatePayload } from './updatePayload.types';
 export { LayoutItem } from './layoutItem.types';
 export { LayoutData } from './layoutData.types';
+export { BlockType } from './blockType.types';
